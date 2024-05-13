a3e8f2d4d4c4161c9c0b26ede6cf3539bffba6ee
Yashchakole1501 <Build@123>
2024-05-13 17:18:25 +0530

    initial commit

---
+++ Ramji/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ramji
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
